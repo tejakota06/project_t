bec100e6d3bea32152c59a74f49879305f0454ff
Teja Kota <tejkota777@gmail.com>
2025-05-28 22:12:34 -0400

    addingfiles

---
+++ basicsAndOops/basics.py
@@ -0,0 +1,16 @@
+#fruit=['apple','banana','orane', 'grape']
+#print(fruit[::2])
+#def greet(name):
+  #  print("Hello,", name)
+
+#greet("Alice")
+def add(a, b): #function Example
+    print(a + b)
+
+result = add(2, 3)  # Prints 5
+print(result)       # Prints None
+
+
+
+#narrowcst example
+#string interpolation example
